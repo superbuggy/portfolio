7228de971026a4991cd7fbe71eb4fe16b0881371
James Reichard <jpqreichard@gmail.com>
2018-07-26 00:00:48 -0400

    Convert H1s to SVG text

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Profile.css'
 import { Moon } from '../Moon/Moon'
+import NameHeader from '../NameHeader/NameHeader'
 
 export const Profile = props => {
   return (
     <div className='profile'>
       <header>
-        <Moon size={128} />
-        <h1>
+        <Moon size={window.innerHeight / 5} />
+        {/* <h1>
           James
         </h1>
-        <h1>Reichard</h1>
+        <h1>Reichard</h1> */}
+        <NameHeader text={'James'} order={1} />
+        <NameHeader text={'Reichard'} order={2} />
       </header>
 
       <div className='description'>
